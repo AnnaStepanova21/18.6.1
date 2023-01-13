964c1c01cebc14d7d7afe61414c692891bbcdea3
AnnaStepanova21 <anna.stepanova69@yandex.ru>
2023-01-13 21:43:50 +0300

    add bot

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '5736923077:AAEA5R9rPAenA_yeMAhT6bNNHExAAYe2d3w'
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'йены': 'JPY',
+    'евро': 'EUR',
+    'рубли': 'RUB'
+}
